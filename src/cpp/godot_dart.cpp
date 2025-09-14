4887b7aeb110ee8023a236dabcf651e5a3d7b3bd
Eric Apostal <ehapostal04@gmail.com>
2025-09-14 17:37:57 -0400

    remove runtime depend

--- src/cpp/godot_dart.cpp
+++ src/cpp/godot_dart.cpp
@@ -3,29 +3,12 @@
 #include <godot_cpp/classes/object.hpp>
 #include <godot_cpp/classes/editor_plugin_registration.hpp>
 
-#include "dart_helpers.h"
-#include "dart_bindings.h"
 #include "gde_wrapper.h"
-#include "godot_string_wrappers.h"
-
-#include "dart_instance_binding.h"
-#include "ref_counted_wrapper.h"
-
-#include "editor/godot_dart_editor_plugin.h"
-#include "editor/dart_progress_dialog.h"
 
 namespace godot_dart {
 
-GodotDartRuntimePlugin *runtime_plugin = nullptr;
 
 void initialize_level(godot::ModuleInitializationLevel p_level) {
-  if (p_level == godot::ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_EDITOR) {
-    godot::ClassDB::register_class<GodotDartEditorPlugin>();
-    godot::ClassDB::register_class<DartProgressDialog>();
-
-    godot::EditorPlugins::add_by_type<GodotDartEditorPlugin>();
-  }
-
   // TODO - Should we setup different types at different times?
   if (p_level != godot::ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE) {
     return;
@@ -37,18 +20,11 @@ void initialize_level(godot::ModuleInitializationLevel p_level) {
     return;
   }
 
-  // Get the library path
-  runtime_plugin = new GodotDartRuntimePlugin();
-  runtime_plugin->base_init();
 }
 
 void deinitialize_level(godot::ModuleInitializationLevel p_level) {
   if (p_level != godot::ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE) {
     return;
-  }
-
-  if (runtime_plugin != nullptr) {
-    runtime_plugin->shutdown_dart_bindings();
   }
 }
 
@@ -94,16 +70,10 @@ void GDE_EXPORT godot_dart_embedded_shutdown() {
 }
 
 bool GDE_EXPORT godot_dart_initialize_runtime() {
-  if (godot_dart::runtime_plugin != nullptr) {
-    return godot_dart::runtime_plugin->initialize_dart_bindings();
-  }
-  return false;
+  return true;
 }
 
 void GDE_EXPORT godot_dart_shutdown_runtime() {
-  if (godot_dart::runtime_plugin != nullptr) {
-    godot_dart::runtime_plugin->shutdown_dart_bindings();
-  }
 }
 
 }
